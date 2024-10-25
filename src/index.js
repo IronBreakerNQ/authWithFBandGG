d3441744182b2a5c4c03d5e498b3a20ff303d0e1
IronBreakerNQ <109438422+IronBreakerNQ@users.noreply.github.com>
2024-10-25 19:14:50 +0700

    Add project files excluding .env

--- src/index.js
+++ src/index.js
@@ -45,5 +45,5 @@ routers(app);
 const PORT = process.env.PORT || 3002;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
